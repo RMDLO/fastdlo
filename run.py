13979416990650d9db8ebca04c31a052a55dbc97
JeffersonAero <holly.dinkel@gmail.com>
2023-01-05 16:51:25 -0600

    loop through all cocopen val data to visualize predictions

--- run.py
+++ run.py
@@ -8,25 +8,29 @@ if __name__ == "__main__":
     dir = "/home/hollydinkel/COCOpen/datasets/cocopen-dataset/val"
     ckpt_siam_name = "CP_similarity.pth"
     ckpt_seg_name = "CP_segmentation.pth"
-    IMG_W = 1920
-    IMG_H = 1080
+    IMG_W = 640
+    IMG_H = 480
     ######################
+    list = os.listdir(dir)
+    for file in list:
+        _, ext = os.path.split(file)
+        if ext[-3:] == "png":
+            script_path = os.path.dirname(os.path.realpath(__file__))
+            checkpoint_siam = os.path.join(script_path, "weights/" + ckpt_siam_name)
+            checkpoint_seg = os.path.join(script_path, "weights/" + ckpt_seg_name)
+            
 
-    for file in os.listdir(dir):
-        script_path = os.path.dirname(os.path.realpath(__file__))
-        checkpoint_siam = os.path.join(script_path, "weights/" + ckpt_siam_name)
-        checkpoint_seg = os.path.join(script_path, "weights/" + ckpt_seg_name)
-        
+            p = Pipeline(checkpoint_siam=checkpoint_siam, checkpoint_seg=checkpoint_seg, img_w=IMG_W, img_h=IMG_H)
 
-        p = Pipeline(checkpoint_siam=checkpoint_siam, checkpoint_seg=checkpoint_seg, img_w=IMG_W, img_h=IMG_H)
+            # COLOR
+            source_img = cv2.imread(f"{dir}/{file}", cv2.IMREAD_COLOR)
+            source_img = cv2.resize(source_img, (IMG_W, IMG_H))
 
-        # COLOR
-        source_img = cv2.imread(f"{dir}/{file}", cv2.IMREAD_COLOR)
-        source_img = cv2.resize(source_img, (IMG_W, IMG_H))
+            img_out, _ = p.run(source_img=source_img, mask_th=77)
 
-        img_out, _ = p.run(source_img=source_img, mask_th=77)
-
-        canvas = source_img.copy()
-        canvas = cv2.addWeighted(canvas, 1.0, img_out, 0.8, 0.0)
-        
-        cv2.imwrite(f"./predictions/{file}",canvas)
+            canvas = source_img.copy()
+            canvas = cv2.addWeighted(canvas, 1.0, img_out, 0.8, 0.0)
+            
+            cv2.imwrite(f"./predictions/{file}",canvas)
+        else:
+            continue
